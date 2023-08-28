d2e4a77da93c8d32d19f2cbf05dc4cd98dcbf63a
Joe Nudell <jnu@stanford.edu>
2023-08-28 12:51:04 -0700

    page extraction and other experiments

--- bc2/bc2.py
+++ bc2/bc2.py
@@ -1,4 +1,5 @@
 import os
+import logging
 
 import click
 from azure.ai.formrecognizer import AnalyzeResult
@@ -8,19 +9,27 @@ from document_analysis import analyze_document, get_output_path
 import llm
 import render
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-def extract_narrative_fields(analysis: list[AnalyzeResult]) -> list[DocumentField]:
+
+def extract_narrative_fields(
+        analysis: list[AnalyzeResult],
+        min_confidence: float = 0.04
+        ) -> list[DocumentField]:
     """Extract the narrative from the analysis results.
 
     Args:
         analysis (list[AnalyzeResult]): Results, one for each page
+        min_confidence (float, optional): Minimum confidence to accept
     """
     narratives = list[str]()
     # Look through each page of the analysis results and find any narratives.
     for page in analysis:
         for doc in page.documents:
             narrative = doc.fields.get("narrative")
-            if narrative:
+            confidence = getattr(narrative, 'confidence', 0.) or 0.
+            if narrative and confidence >= min_confidence:
                 narratives.append(narrative)
     return narratives
 
@@ -92,15 +101,26 @@ def run(path: str, model: str, document_root: str, cache_dir: str | None = None)
         document_root (str): Path to the root of the document.
         cache_dir (str | None): Path to the directory to cache results in.
     """
+    logger.info("Loading Blind Charging redaction tool ...")
     cached = get_output_path(path, document_root, cache_dir, model)
+    logger.info(f"Running analysis on {path} with model {model} ...")
     analysis = analyze_document(path, model=model, cached=cached)
+    logger.info("Inspecting analysis result to find narrative(s) ...")
     fields = extract_narrative_fields(analysis)
+    if not fields:
+        logger.warning("No narrative found in document!")
+        return
     narrative = get_narrative(fields)
+    logger.info("Redacting narrative with language model ...")
     redacted = redact_text(narrative, cached=cached)
+    logger.info("Rendering redacted narrative ...")
+    redact_path = os.path.join(cached, "redacted.pdf")
     render.pdf(
-            os.path.join(cached, "redacted.pdf"),
+            redact_path,
             redacted,
             original=narrative)
+    logger.info(f"Redacted narrative saved to {redact_path}")
+    logger.info("Done!")
 
 
 if __name__ == "__main__":
