a91ac3f7ec8812a1f3ac3c0da72b052819e6c25c
Alex Chohlas-Wood <chohlasa@gmail.com>
2024-01-19 18:21:29 -0800

    Adding script that works on Karpel VM (#9)
    * Adding script that works on Karpel VM
    
    * Trying to fix trailing whitespace
    
    * Try 2 to fix trailing whitespaces
    
    ---------
    
    Co-authored-by: Standford User1 <stfdusr1@BlindCharge.3puah4otg34une24qmvv3s1xeb.snx.internal.usgovcloudapp.net>

---
+++ bc2/extract_pages.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Function to standardize names
+standardize_name() {
+    echo "$1" | tr '[:upper:]' '[:lower:]' | sed 's/[^[:alnum:]]/_/g'
+}
+
+# Check if correct number of arguments are provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <source_folder> <destination_folder>"
+    exit 1
+fi
+
+# Assign source and destination folders
+src_folder="$1"
+dst_folder="$2"
+
+# Recursively find all PDF files in the source folder
+find "$src_folder" -type f -name "*.pdf" | while read -r file; do
+
+    echo $file
+
+    # Get the relative path of the file, excluding the source folder path and the file extension
+    relative_path="${file#$src_folder}"
+    relative_path="${relative_path%.pdf}"
+
+    # Standardize directory names
+    standardized_dir=$(standardize_name "$relative_path")
+
+    # Create a new directory in the destination folder with the same relative path
+    new_dir="$dst_folder$standardized_dir"
+    mkdir -p "$new_dir"
+
+    # Standardize filenames
+    standardized_filename=$(standardize_name "${relative_path##*/}")
+
+    case_id=$(echo "$relative_path" | awk -F'/' '{print $1}')
+
+    pdfseparate "$file" "$new_dir/${case_id}_${relative_path##*/}_page_%03d.pdf"
+
+done
