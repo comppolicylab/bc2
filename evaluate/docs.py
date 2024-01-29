6bb24c27fb4a40f158088726c455271bc6b70862
Joe Nudell <jnudell@hks.harvard.edu>
2024-01-29 14:10:54 -0800

    refactor doc access api for easier use externally

---
+++ evaluate/docs.py
@@ -0,0 +1,112 @@
+import logging
+import queue
+import threading
+
+from .io import FileIO
+
+logger = logging.getLogger(__name__)
+
+
+def list_docs(
+    fr: FileIO, base_path: str, ocr: bool = False, labels: bool = True
+) -> list[str]:
+    """Get a list of files that have labels and OCR.
+
+    Args:
+        fr: A FileIO instance.
+        base_path: The base path to the data.
+        ocr: Filter to files that have OCR.
+        labels: Filter to files that have labels.
+
+    Returns:
+        A list of file names.
+    """
+    all_files = list(fr.list(base_path))
+
+    # Process all the files in the directory
+    pdfs = set[str]()
+    has_labels = set[str]()
+    has_ocr = set[str]()
+
+    for name in all_files:
+        if name.endswith(".pdf"):
+            pdfs.add(name)
+        elif name.endswith(".labels.json"):
+            pdf_name = name[: -len(".labels.json")]
+            has_labels.add(pdf_name)
+        elif name.endswith(".ocr.json"):
+            pdf_name = name[: -len(".ocr.json")]
+            has_ocr.add(pdf_name)
+
+    # Apply filters
+    if ocr:
+        pdfs = pdfs.intersection(has_ocr)
+
+    if labels:
+        pdfs = pdfs.intersection(has_labels)
+
+    # Return names of PDFs
+    return list(pdfs)
+
+
+def copy_doc(fr: FileIO, name: str, dest_dir: str):
+    """Copy the labeled data for the given file to the destination directory.
+
+    Args:
+        fr: A FileIO instance.
+        name: The name of the file to copy.
+        dest_dir: The destination directory.
+    """
+    basename = fr.basename(name)
+
+    for sfx in ["", ".labels.json", ".ocr.json"]:
+        src = name + sfx
+        dst = fr.join(dest_dir, basename + sfx)
+        if fr.exists(src):
+            fr.copy(src, dst)
+
+
+def multi_copy_docs(fr: FileIO, files: list[str], dest_dir: str, threads: int = 1):
+    """Copy all labeled data for the given files to the destination directory.
+
+    Args:
+        fr: A FileIO instance.
+        files: A list of file names.
+        dest_dir: The destination directory.
+        threads: The number of threads to use.
+    """
+    if threads < 1:
+        raise ValueError("threads must be at least 1")
+
+    q = queue.Queue[str]()
+    for name in files:
+        q.put_nowait(name)
+
+    def worker():
+        while True:
+            try:
+                name = q.get(timeout=1)
+                if name is None:
+                    continue
+                logger.debug(f"Copying {name} to {dest_dir} ...")
+                try:
+                    copy_doc(fr, name, dest_dir)
+                except KeyboardInterrupt:
+                    logger.warning("Keyboard interrupt, exiting thread ...")
+                    break
+                except Exception as e:
+                    logger.warning(f"Failed to copy {name}: {e}, retrying ...")
+                    # TODO: cap retries
+                    q.put(name)
+                finally:
+                    q.task_done()
+            except queue.Empty:
+                break
+
+    logger.debug(f"Starting {threads} threads to copy data ...")
+    tx = [threading.Thread(target=worker) for _ in range(threads)]
+    for t in tx:
+        t.start()
+    q.join()
+    for t in tx:
+        t.join()
--- evaluate/fold.py
+++ evaluate/fold.py
@@ -1,11 +1,10 @@
 import json
 import logging
-import queue
-import threading
 from dataclasses import asdict, dataclass
 from datetime import datetime
 from typing import TypedDict
 
+from .docs import list_docs, multi_copy_docs
 from .io import FileIO
 from .sample import KFoldCrossValidationSampler, RegExMatchGrouper
 
@@ -36,101 +35,6 @@ class Metadata:
     files: list[str]
 
 
-def get_labeled_files(fr: FileIO, base_path: str) -> list[str]:
-    """Get a list of files that have labels and OCR.
-
-    Args:
-        fr: A FileIO instance.
-        base_path: The base path to the data.
-
-    Returns:
-        A list of file names.
-    """
-    all_files = list(fr.list(base_path))
-
-    # Filter to files that have OCR and labels.
-    pdfs = set[str]()
-    has_labels = set[str]()
-    has_ocr = set[str]()
-
-    for name in all_files:
-        if name.endswith(".pdf"):
-            pdfs.add(name)
-        elif name.endswith(".labels.json"):
-            pdf_name = name[: -len(".labels.json")]
-            has_labels.add(pdf_name)
-        elif name.endswith(".ocr.json"):
-            pdf_name = name[: -len(".ocr.json")]
-            has_ocr.add(pdf_name)
-
-    return list(pdfs.intersection(has_ocr))
-
-
-def copy_labeled_data(fr: FileIO, name: str, dest_dir: str):
-    """Copy the labeled data for the given file to the destination directory.
-
-    Args:
-        fr: A FileIO instance.
-        name: The name of the file to copy.
-        dest_dir: The destination directory.
-    """
-    basename = fr.basename(name)
-
-    for sfx in ["", ".labels.json", ".ocr.json"]:
-        src = name + sfx
-        dst = fr.join(dest_dir, basename + sfx)
-        if fr.exists(src):
-            fr.copy(src, dst)
-
-
-def multi_copy_labeled_data(
-    fr: FileIO, files: list[str], dest_dir: str, threads: int = 1
-):
-    """Copy all labeled data for the given files to the destination directory.
-
-    Args:
-        fr: A FileIO instance.
-        files: A list of file names.
-        dest_dir: The destination directory.
-        threads: The number of threads to use.
-    """
-    if threads < 1:
-        raise ValueError("threads must be at least 1")
-
-    q = queue.Queue[str]()
-    for name in files:
-        q.put_nowait(name)
-
-    def worker():
-        while True:
-            try:
-                name = q.get(timeout=1)
-                if name is None:
-                    continue
-                logger.debug(f"Copying {name} to {dest_dir} ...")
-                try:
-                    copy_labeled_data(fr, name, dest_dir)
-                except KeyboardInterrupt:
-                    logger.warning("Keyboard interrupt, exiting thread ...")
-                    break
-                except Exception as e:
-                    logger.warning(f"Failed to copy {name}: {e}, retrying ...")
-                    # TODO: cap retries
-                    q.put(name)
-                finally:
-                    q.task_done()
-            except queue.Empty:
-                break
-
-    logger.debug(f"Starting {threads} threads to copy data ...")
-    tx = [threading.Thread(target=worker) for _ in range(threads)]
-    for t in tx:
-        t.start()
-    q.join()
-    for t in tx:
-        t.join()
-
-
 def fold(
     fr: FileIO,
     doc_base_path: str,
@@ -159,7 +63,7 @@ def fold(
     eval_dir = fr.join(eval_base_path, eval_name)
     logger.info(f"Creating evaluation directory {eval_dir} ...")
 
-    files_to_process = get_labeled_files(fr, doc_base_path)
+    files_to_process = list_docs(fr, doc_base_path, ocr=True, labels=True)
 
     logger.info(f"Found {len(files_to_process)} files in {doc_base_path}")
     kfold = KFoldCrossValidationSampler[str](k)
@@ -183,8 +87,8 @@ def fold(
         fold_dir = fr.join(eval_dir, f"fold-{i}")
         train_dir = fr.join(fold_dir, "train")
         test_dir = fr.join(fold_dir, "test")
-        multi_copy_labeled_data(fr, train, train_dir, threads=threads)
-        multi_copy_labeled_data(fr, test, test_dir, threads=threads)
+        multi_copy_docs(fr, train, train_dir, threads=threads)
+        multi_copy_docs(fr, test, test_dir, threads=threads)
         metadata.folds.append(
             FoldDetail(
                 id=f"{eval_name}-fold-{i}",
