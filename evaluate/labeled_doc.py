3a7b2c20a357e51a77dff53db1ecf718bbe7e8b0
Joe Nudell <jnudell@hks.harvard.edu>
2024-04-29 12:45:52 -0400

    fix broken schema parsing (#15)

--- evaluate/labeled_doc.py
+++ evaluate/labeled_doc.py
@@ -1,29 +1,34 @@
-from functools import cache
+from typing import Literal, Union
 
-import python_jsonschema_objects as pjs
-import requests
+from pydantic import BaseModel, conlist
 
-
-@cache
-def get_pjs_ns(schema_url: str):
-    """Get a Python JSON Schema namespace from a schema URL."""
-    schema = requests.get(schema_url, verify=False).json()
-
-    # XXX(jnu): apparently the schema changed?? or is wrong??
-    # This ugly hack is required all of a sudden to make my documents parse...
-    # The error is that the value is not a json-pointer, but
-    # the schema expects it to be ?????
-    # NOTE that there's a good chance the schema changed and new files that
-    # are created will not work with this hack ...
-    schema["definitions"]["Label"]["properties"]["label"].pop("format")
-    builder = pjs.ObjectBuilder(schema)
-    ns = builder.build_classes()
-    return ns
+# NOTE: The DocumentIntelligence *.labels.json files advertise a JSON schema
+# at the following URL:
+# https://schema.cognitiveservices.azure.com/formrecognizer/2021-03-01/labels.json
+#
+# Unfortunately, as of 2024-04-29 that schema no longer exists. Previously, we
+# parsed the files according to that JSON schema, but now we just do our own
+# parsing. This means we won't catch breaking schema changes, so be careful!
 
 
-schema = (
-    "https://schema.cognitiveservices.azure.com/formrecognizer/2021-03-01/labels.json"
-)
-ns = get_pjs_ns(schema)
+BoundingBox = conlist(float, min_length=8, max_length=8)
 
-LabeledDoc = ns.LabelsJsonSchema
+
+class LabelRegionValue(BaseModel):
+    page: int
+    text: str
+    boundingBoxes: list[list[float]] = BoundingBox
+
+
+class LabelRegion(BaseModel):
+    label: str
+    value: list[LabelRegionValue]
+    labelType: Literal["region"]
+
+
+Label = Union[LabelRegion]
+
+
+class LabeledDoc(BaseModel):
+    document: str
+    labels: list[Label]
