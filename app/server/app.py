4fa6865b60cf165bc713117f9e281e67a5268c6e
Joe Nudell <jnudell@hks.harvard.edu>
2024-05-10 12:34:20 -0700

    cli and db models placeholder

--- app/server/app.py
+++ app/server/app.py
@@ -1,7 +1,36 @@
-from fastapi import FastAPI
+import logging
 
+from fastapi import FastAPI, Request
+
+from .config import config
 from .generated import app as generated_app
 
-app = FastAPI()
+logger = logging.getLogger(__name__)
+
+
+async def lifespan():
+    """Create the database if it doesn't exist."""
+    if not await config.db.driver.exists():
+        logger.warning("No database exists, creating a new one")
+        await config.db.driver.create()
+
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+@app.middleware("http")
+async def begin_db_session(request: Request, call_next):
+    async with config.db.driver.async_session() as session:
+        request.state.db = session
+        try:
+            response = await call_next(request)
+            session.commit()
+            return response
+        except Exception as e:
+            session.rollback()
+            raise e
+
 
 app.mount("/api/v1", generated_app)
---
+++ app/server/config.py
@@ -0,0 +1,47 @@
+import os
+from functools import cached_property
+from pathlib import Path
+from typing import Literal, Union
+
+import tomllib
+from glowplug import MsSqlDriver
+from pydantic import BaseSettings
+
+
+class MsSqlConfig(BaseSettings):
+    dialect: Literal["mssql"]
+    odbc_driver: Literal["ODBC Driver 17 for SQL Server"]
+    host: str
+    port: int
+    user: str
+    password: str
+    database: str
+
+    @cached_property
+    def driver(self):
+        odbc_driver = self.odbc_driver.replace(" ", "+")
+        path = (
+            f"{self.user}:{self.password}@{self.host}:{self.port}"
+            f"/{self.database}?driver={odbc_driver}"
+        )
+        return MsSqlDriver(path)
+
+
+DbConfig = Union[MsSqlConfig]
+
+
+class Config(BaseSettings):
+    db: DbConfig
+
+
+def _load_config(path: str = os.getenv("CONFIG_PATH", "config.toml")) -> Config:
+    """Load the configuration from a TOML file."""
+    if not Path(path).exists():
+        raise FileNotFoundError(f"Config file not found: {path}")
+    raw_cfg = Path(path).read_text()
+    cfg = tomllib.loads(raw_cfg)
+    return Config.model_validate(cfg)
+
+
+# The global app configuration
+config = _load_config()
