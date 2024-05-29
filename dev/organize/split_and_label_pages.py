9ce51e1c08a5be842f47f494e519abf5df447429
Alex Chohlas-Wood <achohlaswood@hks.harvard.edu>
2024-05-29 09:17:14 -0700

    Working version to demo to Muskan

--- dev/organize/split_and_label_pages.py
+++ dev/organize/split_and_label_pages.py
@@ -9,22 +9,21 @@ def format_filename(text):
     text = re.sub(r'[^a-z0-9_]', '', text)
     return text
 
-def process_pdf(case_id, filename, agency, state,
+def process_pdf(folder_name, filename, case_id, agency, state,
                 rows, source_folder, dest_folder):
     
-    redacted_filename = filename.replace(".pdf", "_r.pdf")
-
+    print(f"Processing {agency} - {state} - {folder_name} - {filename} - {case_id}")
     agency_filename = format_filename(agency)
     agency_folder = f"{state.lower()}_{agency_filename}"
     pdf_path = os.path.join(source_folder, agency_folder,
-                            str(case_id), str(redacted_filename))
+                            "raw", folder_name, str(filename))
 
     with open(pdf_path, 'rb') as file:
         pdf_reader = PdfReader(file)
 
         for _, row in rows.iterrows():
             doc_attached_pages = []
-            doc_attached_pages_value = row['Attached_Pages']
+            doc_attached_pages_value = row['foreign_pages']
             if not pd.isnull(doc_attached_pages_value):
                 print(doc_attached_pages_value)
                 print()
@@ -32,24 +31,25 @@ def process_pdf(case_id, filename, agency, state,
                 for item in attached_ranges:
                     if '-' in item:
                         start, end = item.split('-')
-                        doc_attached_pages.extend(range(int(start), int(end) + 1))
+                        doc_attached_pages.extend(range(int(start), 
+                                                        int(end) + 1))
                     else:
                         doc_attached_pages.append(int(item))
 
-            doc_start = row['Document_Start']
-            doc_end = row['Document_End']
-            doc_type = row['Document_Type'].lower()
-            doc_id = row['Document_ID']
-            doc_num = row['Document_Num']
-            group = row['Group']
+            doc_start = row['document_start']
+            doc_end   = row['document_end']
+            doc_type  = row['document_type'].lower().strip()
+            # doc_num   = row['document_num']
 
             for page in range(int(doc_start) - 1, int(doc_end)):
                 if page + 1 in doc_attached_pages:
                     continue
-                output_filename = f"{case_id}_{doc_id}_{doc_num}_pg{page + 1:03}.pdf"
-                group_lowercase = group.lower()
-                output_path = os.path.join(dest_folder, agency_folder,
-                                           group_lowercase, output_filename)
+                output_filename = "__".join([agency_folder, folder_name, 
+                                             str(filename), 
+                                            #  str(case_id), 
+                                            #  str(doc_num), doc_type, 
+                                             f"pg{page + 1:03}"]) + ".pdf"
+                output_path = os.path.join(dest_folder, doc_type, output_filename)
                 os.makedirs(os.path.dirname(output_path), exist_ok=True)
 
                 pdf_writer = PdfWriter()
@@ -58,32 +58,31 @@ def process_pdf(case_id, filename, agency, state,
                 with open(output_path, 'wb') as output:
                     print(f"Writing {output_filename}")
                     pdf_writer.write(output)
+    print("")
 
-def main(directory_path, groups, source_agency, source_folder, dest_folder):
+def main(directory_path, doc_type, source_folder, dest_folder):
     df = pd.read_excel(directory_path)
-    filtered_df = df[df['Group'].str.contains(groups, regex=True) & 
-                     df['Referring_Agency'].str.contains(source_agency, 
-                                                         regex=True) &
-                     df['Authoring_Agency'].str.contains(source_agency, 
-                                                         regex=True)]
-    print(len(filtered_df))
-    grouped_df = filtered_df.groupby(['Case_ID', 'Filename', 
-                                      'Referring_Agency', 
-                                      'Referring_Agency_State'])
+    filtered_df = df[df['document_type'].str.contains(doc_type, regex=True, na=False) & 
+                     (df['duplicate_notes'] != "Ignore")]
 
-    for (case_id, filename, agency, state), group_df in grouped_df:
-        process_pdf(case_id, filename, agency, state, 
+    print(len(df))
+
+    grouped_df = filtered_df.groupby(['folder_name', 'file_name', 'document_id', 
+                                      'referring_agency', 
+                                      'referring_agency_state'])
+
+    for (folder_name, file_name, document_id, agency, state), group_df in grouped_df:
+        process_pdf(folder_name, file_name, document_id, agency, state, 
                     group_df, source_folder, dest_folder)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process PDF files based on Excel data.')
     parser.add_argument('directory_path', type=str, help='Path to the Excel file')
-    parser.add_argument('groups', type=str, help='Regex string for filtering the Groups column')
-    parser.add_argument('source_agency', type=str, help='Regex string for filtering the agency columns')
+    parser.add_argument('doc_type', type=str, help='Regex string for filtering the Groups column')
     parser.add_argument('source_folder', type=str, help='Path to the source folder')
     parser.add_argument('dest_folder', type=str, help='Path to the destination folder')
 
     args = parser.parse_args()
 
-    main(args.directory_path, args.groups, args.source_agency, 
+    main(args.directory_path, args.doc_type, 
          args.source_folder, args.dest_folder)
