04e77cbac433c329924d7075e8f235c750dfd04d
Joe Nudell <jnudell@hks.harvard.edu>
2024-07-16 12:05:18 -0400

    fix bug with None prompt value (#64)

---
+++ lib/blind_charging_core/common/test_openai.py
@@ -0,0 +1,101 @@
+import tempfile
+
+from .openai import (
+    OpenAIChatInputText,
+    OpenAIChatPromptFile,
+    OpenAIChatPromptInline,
+    OpenAIChatTurn,
+)
+
+
+def test_format_prompt_inline_jinja():
+    c = OpenAIChatPromptInline(prompt="Hello, {{ alias }}!", engine="jinja")
+    assert c.format("How are you?", alias="world") == [
+        OpenAIChatTurn(
+            role="system",
+            content="Hello, world!",
+        ),
+        OpenAIChatTurn(
+            role="user",
+            content=[OpenAIChatInputText(text="How are you?", type="text")],
+        ),
+    ]
+
+
+def test_format_prompt_inline_string():
+    c = OpenAIChatPromptInline(prompt="Hello, {alias}!", engine="string")
+    assert c.format("How are you?", alias="world") == [
+        OpenAIChatTurn(
+            role="system",
+            content="Hello, world!",
+        ),
+        OpenAIChatTurn(
+            role="user",
+            content=[OpenAIChatInputText(text="How are you?", type="text")],
+        ),
+    ]
+
+
+def test_format_prompt_inline_string_no_placeholder():
+    c = OpenAIChatPromptInline(prompt="Hello!", engine="string")
+    assert c.format("How are you?", alias="world") == [
+        OpenAIChatTurn(
+            role="system",
+            content="Hello!\nworld",
+        ),
+        OpenAIChatTurn(
+            role="user",
+            content=[OpenAIChatInputText(text="How are you?", type="text")],
+        ),
+    ]
+
+
+def test_format_prompt_file_jinja():
+    with tempfile.NamedTemporaryFile(mode="w") as f:
+        f.write("Hello, {{ alias }}!")
+        f.flush()
+        c = OpenAIChatPromptFile(prompt_file=f.name, engine="jinja")
+        assert c.format("How are you?", alias="world") == [
+            OpenAIChatTurn(
+                role="system",
+                content="Hello, world!",
+            ),
+            OpenAIChatTurn(
+                role="user",
+                content=[OpenAIChatInputText(text="How are you?", type="text")],
+            ),
+        ]
+
+
+def test_format_prompt_file_string():
+    with tempfile.NamedTemporaryFile(mode="w") as f:
+        f.write("Hello, {alias}!")
+        f.flush()
+        c = OpenAIChatPromptFile(prompt_file=f.name, engine="string")
+        assert c.format("How are you?", alias="world") == [
+            OpenAIChatTurn(
+                role="system",
+                content="Hello, world!",
+            ),
+            OpenAIChatTurn(
+                role="user",
+                content=[OpenAIChatInputText(text="How are you?", type="text")],
+            ),
+        ]
+
+
+def test_format_prompt_file_string_no_placeholder():
+    with tempfile.NamedTemporaryFile(mode="w") as f:
+        f.write("Hello!")
+        f.flush()
+        c = OpenAIChatPromptFile(prompt_file=f.name, engine="string")
+        assert c.format("How are you?", alias="world") == [
+            OpenAIChatTurn(
+                role="system",
+                content="Hello!\nworld",
+            ),
+            OpenAIChatTurn(
+                role="user",
+                content=[OpenAIChatInputText(text="How are you?", type="text")],
+            ),
+        ]
