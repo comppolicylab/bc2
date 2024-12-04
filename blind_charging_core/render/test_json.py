f12891425b58eac80cf80f0f5528ed4f1f90cf4c
Joe Nudell <jnudell@hks.harvard.edu>
2024-12-03 22:01:52 -0500

    include annotations in json output

--- blind_charging_core/render/test_json.py
+++ blind_charging_core/render/test_json.py
@@ -8,19 +8,44 @@ from .json import JsonRenderConfig
 
 
 @pytest.mark.parametrize(
-    "original, redacted",
+    "original, redacted, annotations",
     [
         (
             "Hello, world!",
             "Hello, [Person 1]!",
+            [
+                {
+                    "originalSpan": [7, 12],
+                    "redactedSpan": [7, 17],
+                    "valid": True,
+                    "openDelim": "[",
+                    "closeDelim": "]",
+                }
+            ],
         ),
         (
-            "I took photos of Leopold ears.",
-            "I took photos of [Subject 1]'s ears.",
+            "I took photos of Leopold's ears.",
+            "I took photos of [Subject 1]'s ears!",
+            [
+                {
+                    "originalSpan": [17, 24],
+                    "redactedSpan": [17, 28],
+                    "valid": True,
+                    "openDelim": "[",
+                    "closeDelim": "]",
+                },
+                {
+                    "originalSpan": [31, 32],
+                    "redactedSpan": [35, 36],
+                    "valid": False,
+                    "openDelim": None,
+                    "closeDelim": None,
+                },
+            ],
         ),
     ],
 )
-def test_render_json(original, redacted):
+def test_render_json(original, redacted, annotations):
     cfg = JsonRenderConfig.model_validate(
         {
             "engine": "render:json",
@@ -33,4 +58,5 @@ def test_render_json(original, redacted):
     assert json.loads(result.content()) == {
         "redacted": redacted,
         "original": original,
+        "annotations": annotations,
     }
