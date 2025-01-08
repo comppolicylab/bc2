f71e496d4e8aff0af44245ec66188a3c8a3c886b
Alex Chohlas-Wood <chohlasa@gmail.com>
2025-01-08 14:24:09 -0500

    Prep for open source (#80)
    * Remove dev code
    
    * Updating footer to add feedback form
    
    * Removing jupyter
    
    * Formatting tweaks in response to feedback

--- blind_charging_core/render/test_text.py
+++ blind_charging_core/render/test_text.py
@@ -13,8 +13,8 @@ def decorate(txt: str) -> bytes:
         b"The above passages were automatically extracted from referral "
         b"documents and automatically redacted to hide race-related "
         b"information. Occasionally, words or punctuation may be "
-        b"automatically added to fix typos. Please report any issues to "
-        b"blind_charging@hks.harvard.edu."
+        b"automatically added to fix typos. Please report any issues at "
+        b"https://bit.ly/report-rbc-bug."
     )
     return header + txt.encode("utf-8") + footer
 
