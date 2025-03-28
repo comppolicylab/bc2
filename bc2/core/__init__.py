7ebb76b3513b1579557e9e4d73ed5100feeae1de
Joe Nudell <jnudell@hks.harvard.edu>
2025-03-28 13:28:54 -0400

    make types more accessible (#89)

--- bc2/core/__init__.py
+++ bc2/core/__init__.py
@@ -1,5 +1,35 @@
+import typing
+
+from .common.all import AnyConfig as _AnyConfig
+from .common.all import AnyIOConfig as _AnyIOConfig
+from .common.all import AnyProcessingConfig as _AnyProcessingConfig
 from .common.name_map import IdToMaskMap, IdToNameMap, NameToMaskMap
 from .pipeline import Pipeline, PipelineConfig
+
+# MyPy doesn't like using `typing._eval_type` to evaluate the forward references.
+# We silence the error here.
+#
+# The reason we do this is to allow library users to import these types for use as
+# Pydantic models. Since our types are recursive and use forward references, we need
+# to resolve them before Pydantic can use them.
+
+AnyConfig = typing._eval_type(  # type: ignore[attr-defined]
+    _AnyConfig,
+    globals(),
+    locals(),
+)
+
+AnyProcessingConfig = typing._eval_type(  # type: ignore[attr-defined]
+    _AnyProcessingConfig,
+    globals(),
+    locals(),
+)
+
+AnyIOConfig = typing._eval_type(  # type: ignore[attr-defined]
+    _AnyIOConfig,
+    globals(),
+    locals(),
+)
 
 __all__ = [
     "Pipeline",
@@ -7,4 +37,7 @@ __all__ = [
     "NameToMaskMap",
     "IdToNameMap",
     "IdToMaskMap",
+    "AnyConfig",
+    "AnyProcessingConfig",
+    "AnyIOConfig",
 ]
--- bc2/core/inspect/__init__.py
+++ bc2/core/inspect/__init__.py
@@ -1,12 +1,14 @@
 from typing import Union
 
 from .annotations import AnnotationsInspectConfig
+from .embed import EmbedInspectConfig
 from .masked_subjects import OpenAIMaskedSubjectsInspectConfig
 from .placeholders import OpenAIPlaceholdersInspectConfig
 from .quality import InspectQualityConfig
 
 InspectConfig = Union[
     AnnotationsInspectConfig,
+    EmbedInspectConfig,
     OpenAIPlaceholdersInspectConfig,
     OpenAIMaskedSubjectsInspectConfig,
     InspectQualityConfig,
