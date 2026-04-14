8b6ce54e120ca1a52cecd45cd618d13e86a176d2
Joe Nudell <jnudell@hks.harvard.edu>
2026-04-14 10:45:40 -0400

    Refactor DI from extraction step into analyze+extract (#110)
    * update extract di procedure to prep for onto parsing
    
    * allow explicit mime-type specification
    
    * refactor to break out analyze step
    
    * restore locale

--- bc2/core/extract/azuredi.py
+++ bc2/core/extract/azuredi.py
@@ -1,15 +1,14 @@
 import logging
 from functools import cached_property
-from io import BytesIO
 from typing import Literal, Tuple
 
-from azure.ai.formrecognizer import AnalyzeResult, DocumentAnalysisClient
-from azure.core.credentials import AzureKeyCredential
+from azure.ai.formrecognizer import AnalyzeResult
 from pydantic import BaseModel, Field
-from pypdf import PdfReader
 
 from ..common.file import MemoryFile
-from .base import BaseExtractDriver, register_preprocessor
+from ..common.json import date_aware_json_load
+from ..common.preprocess import register_preprocessor
+from .base import BaseExtractDriver
 
 logger = logging.getLogger(__name__)
 
@@ -18,40 +17,32 @@ class AzureDIExtractConfig(BaseModel):
     """Azure DI Extract config."""
 
     engine: Literal["extract:azuredi"]
-    endpoint: str
-    api_key: str
-    # Todo: Add api_version, since we'll need to match what's on GovCloud,
-    # which has more limited releases than commerical Azure.
-    document_model: str = Field("prebuilt-read")
     labels: list[str] | None = Field(None)
     min_confidence: float = Field(0.04)
-    locale: str = Field("en-US")
 
     @cached_property
     def driver(self) -> "AzureDIExtract":
         return AzureDIExtract(self)
 
 
-class AzureDIExtract(BaseExtractDriver):
+class AzureDIExtract(BaseExtractDriver[AnalyzeResult]):
     def __init__(self, config: AzureDIExtractConfig):
         self.config = config
-        self.document_analysis_client = DocumentAnalysisClient(
-            endpoint=config.endpoint,
-            credential=AzureKeyCredential(config.api_key),
-        )
 
-    @register_preprocessor(r"^application/pdf")
-    def convert_pdf(self, file: MemoryFile) -> BytesIO:
+    @register_preprocessor(r"^application/x-analyze-result")
+    def load_analyze_result(self, file: MemoryFile) -> AnalyzeResult:
         file.buffer.seek(0)
-        return file.buffer
+        # Parse JSON and inflate the AnalyzeResult object.
+        content = date_aware_json_load(file.buffer)
+        return AnalyzeResult.from_dict(content)
 
-    def extract(self, doc: BytesIO) -> Tuple[str, bool]:
-        result = self._extract_text_from_doc(doc) or ""
+    def extract(self, analysis: AnalyzeResult) -> Tuple[str, bool]:
+        result = self._extract_text_from_analysis(analysis) or ""
         return result, False
 
     def _extract_document_content(
         self,
-        analysis: list[AnalyzeResult],
+        analysis: AnalyzeResult,
         labels: list[str] | None = None,
     ) -> list[str]:
         """Extract text spans from the analysis results.
@@ -75,38 +66,36 @@ class AzureDIExtract(BaseExtractDriver):
         chunks = list[str]()
         labels_filter = set(labels) if labels else None
         # Look through each page of the analysis results and find any text.
-        for page in analysis:
-            # If a labels filter was passed, use that to extract only labeled regions.
-            if labels_filter:
-                for doc in page.documents:
-                    for label, field in doc.fields.items():
-                        if label in labels_filter:
-                            confidence = getattr(field, "confidence", 0.0) or 0.0
-                            if (
-                                field
-                                and field.content
-                                and confidence >= self.config.min_confidence
-                            ):
-                                chunks.append(field.content)
-            # When no filter was passed, get text from paragraphs in sequence instead.
-            else:
-                for para in page.paragraphs:
-                    if para.content:
-                        chunks.append(para.content)
+        # If a labels filter was passed, use that to extract only labeled regions.
+        if labels_filter:
+            for doc in analysis.documents or []:
+                for label, field in doc.fields.items():
+                    if label in labels_filter:
+                        confidence = getattr(field, "confidence", 0.0) or 0.0
+                        if (
+                            field
+                            and field.content
+                            and confidence >= self.config.min_confidence
+                        ):
+                            chunks.append(field.content)
+        # When no filter was passed, get text from paragraphs in sequence instead.
+        else:
+            for para in analysis.paragraphs or []:
+                if para.content:
+                    chunks.append(para.content)
         return chunks
 
-    def _extract_text_from_doc(
-        self, doc: BytesIO, chunk_separator: str = "\n\n"
+    def _extract_text_from_analysis(
+        self, analysis: AnalyzeResult, chunk_separator: str = "\n\n"
     ) -> str | None:
-        """Extract text from a PDF.
+        """Extract text from an analysis result.
 
         Args:
-            doc (BytesIO): Stream containing the PDF.
+            analysis (AnalyzeResult): Analysis result from Azure DI
 
         Returns:
             str: The text, if text was found.
         """
-        analysis = self._analyze_document(doc)
         chunks = self._extract_document_content(analysis, self.config.labels)
 
         if not chunks:
@@ -114,38 +103,3 @@ class AzureDIExtract(BaseExtractDriver):
             return None
 
         return chunk_separator.join(chunks)
-
-    def _analyze_document(
-        self,
-        doc: BytesIO,
-    ) -> list[AnalyzeResult]:
-        """Run a PDF through Azure document analysis.
-
-        Args:
-            doc (BytesIO): The PDF to analyze.
-
-        Returns:
-            list[AnalyzeResult]: Results from Azure document analysis.
-        """
-        logger.info(f"Running analysis with model {self.config.document_model} ...")
-        # We analyze each page separately because the FormRecognizer API doesn't
-        # currently fully support entire document analysis. It just analyzes two
-        # pages at a time, even if you request more.
-        # TODO(jnu): check if this is still true?
-        pages = len(PdfReader(doc).pages)
-        results: list[AnalyzeResult] = [None] * pages
-
-        # Run analysis on the document using the remote service.
-        doc.seek(0)
-        docbytes = doc.read()
-        for i in range(pages):
-            if results[i] is None:
-                poller = self.document_analysis_client.begin_analyze_document(
-                    self.config.document_model,
-                    document=docbytes,
-                    locale=self.config.locale,
-                    pages=f"{i + 1}",
-                )
-                results[i] = poller.result()
-
-        return results
--- bc2/core/extract/base.py
+++ bc2/core/extract/base.py
@@ -1,12 +1,9 @@
-import logging
-import re
 from abc import ABC, abstractmethod
-from typing import Callable, Generic, Tuple, TypeVar, cast
+from typing import Generic, Tuple, TypeVar
 
 from ..common.file import MemoryFile
+from ..common.preprocess import PreprocessMixin
 from ..common.text import Text
-
-logger = logging.getLogger(__name__)
 
 T = TypeVar("T")
 
@@ -17,34 +14,7 @@ class EmptyExtractionError(Exception):
     pass
 
 
-class MissingPreprocessorError(Exception):
-    """Raised when no preprocessor is found for the given file type."""
-
-    pass
-
-
-class Preprocessor(ABC, Generic[T]):
-    mime_type: re.Pattern
-
-    @abstractmethod
-    def __call__(self, file: MemoryFile) -> T: ...
-
-    @abstractmethod
-    def __name__(self) -> str: ...
-
-
-def register_preprocessor(mime: str):
-    """Decorator to set the mime type of the processor."""
-
-    def decorator(func: Callable[[MemoryFile], T]) -> Preprocessor[T]:
-        preprocessor = cast(Preprocessor[T], func)
-        preprocessor.mime_type = re.compile(mime)
-        return preprocessor
-
-    return decorator
-
-
-class BaseExtractDriver(ABC, Generic[T]):
+class BaseExtractDriver(ABC, Generic[T], PreprocessMixin[T]):
     def __call__(self, file: MemoryFile) -> Text:
         """Extract the narrative from the given file.
 
@@ -63,33 +33,6 @@ class BaseExtractDriver(ABC, Generic[T]):
         if not text:
             raise EmptyExtractionError("No text found in file.")
         return Text(text, truncated=is_truncated)
-
-    def preprocess(self, file: MemoryFile) -> T:
-        """Preprocess the file before extraction.
-
-        Args:
-            file (MemoryFile): The file to preprocess.
-
-        Returns:
-            T - Intermediate data for extraction
-        """
-        # Look through available methods to find a matching mime type
-        for method in dir(self):
-            fn = getattr(self, method)
-            if not hasattr(fn, "mime_type"):
-                continue
-            preprocessor = cast(Preprocessor[T], fn)
-            pattern = preprocessor.mime_type
-            if pattern.match(file.mime_type):
-                logger.debug(
-                    "Using preprocessor '%s' for file type '%s'",
-                    preprocessor.__name__,
-                    file.mime_type,
-                )
-                return preprocessor(file)
-        raise MissingPreprocessorError(
-            f"Missing preprocessor for type '{file.mime_type}'"
-        )
 
     @abstractmethod
     def extract(self, data: T) -> Tuple[str, bool]:
