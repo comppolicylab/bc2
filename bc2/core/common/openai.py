e51ed3c07594d633fabc0b134be2d7c872050ea1
Joe Nudell <jnudell@hks.harvard.edu>
2026-06-24 13:17:27 -0400

    support for reasoning effort

--- bc2/core/common/openai.py
+++ bc2/core/common/openai.py
@@ -399,6 +399,7 @@ class OpenAIChatConfig(BaseModel, Generic[TResult]):
     )
     temperature: float | None = None
     top_p: float | None = None
+    reasoning_effort: Literal["low", "medium", "high", "xhigh"] | None = None
 
     extender: None = Field(
         None,
@@ -481,6 +482,7 @@ class OpenAIChatConfig(BaseModel, Generic[TResult]):
             "seed",
             "temperature",
             "top_p",
+            "reasoning_effort",
         }
 
         # Only keep populated settings that are in the OpenAI API params list.
@@ -493,6 +495,11 @@ class OpenAIChatConfig(BaseModel, Generic[TResult]):
             if k in openai_api_params and v is not None:
                 openai_api_settings[k] = v
                 continue
+
+        # Reasoning is actually a nested parameter, so handle accordingly.
+        reasoning_effort = openai_api_settings.pop("reasoning_effort", None)
+        if reasoning_effort:
+            openai_api_settings["reasoning"] = {"effort": reasoning_effort}
 
         # Format chat message
         messages = [m.as_chat_message() for m in self.system.format(input, **kwargs)]
